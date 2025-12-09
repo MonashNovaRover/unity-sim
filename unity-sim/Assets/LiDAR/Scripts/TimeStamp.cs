990c0472a70e9e5aa5b5b5ebeab6df459fa6f01b
Jakob-W-Lucas <jluc0013@student.monash.edu>
2025-12-09 19:52:05 +1100

    Refactor TF publisher and LiDAR scanner; remove FLU dependency stubs

---
+++ unity-sim/Assets/LiDAR/Scripts/TimeStamp.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+using RosSharp.RosBridgeClient.MessageTypes.Std;
+using Time = RosSharp.RosBridgeClient.MessageTypes.BuiltinInterfaces.Time;
+
+public static class TimeStamp
+{
+    public static Header GetHeader(string frameId)
+    {
+        var timeInSeconds = UnityEngine.Time.timeAsDouble;
+        var secs = (int)timeInSeconds;
+        var nsecs = (uint)((timeInSeconds - secs) * 1e9);
+        var stamp = new Time(secs, nsecs);
+        
+        return new Header(stamp, frameId);
+    }
+    
+    public static Header GetHeader(double time, string frameId)
+    {
+        var timeInSeconds = time;
+        var secs = (int)timeInSeconds;
+        var nsecs = (uint)((timeInSeconds - secs) * 1e9);
+        var stamp = new Time(secs, nsecs);
+        
+        return new Header(stamp, frameId);
+    }
+}
