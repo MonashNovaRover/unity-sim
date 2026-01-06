495397789076e03db3f1dca919e359e69b01c32e
Jakob-W-Lucas <jluc0013@student.monash.edu>
2026-01-06 15:12:56 +1100

    fix: added docs to LockTransform and KeyboardCameraMovement

--- unity-sim/Assets/Scripts/LockTransform.cs
+++ unity-sim/Assets/Scripts/LockTransform.cs
@@ -1,8 +1,15 @@
 using UnityEngine;
 
+/// <summary>
+/// Locks a gameObject's position and rotation to another transform.
+/// Helpful for objects that rely on being attached to the rover without
+/// specifically changing the rovers joints.
+/// </summary>
 public class LockTransform : MonoBehaviour
 {
     [SerializeField] private Transform _bind;
+    
+    // Able to offset from the exact position of the bound transform
     [SerializeField] private Vector3 _offset;
     private void Update()
     {
