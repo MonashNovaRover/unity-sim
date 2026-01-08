e34ba4f3b9545879b239af054a01b94a40f5ab37
Monash Nova Rover <novaroverteam@monash.edu>
2026-01-08 15:14:18 +1100

    Edited map and improve drive code

--- unity-sim/Assets/Scripts/Rover.cs
+++ unity-sim/Assets/Scripts/Rover.cs
@@ -1,15 +1,22 @@
+/*
+   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+   Monash Nova Rover Team
+   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+   CREATION:	23/12/2025
+   CREATED BY:  Joel Kruger
+   EDITED:      08/01/2026
+   EDITED BY:
+   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+
 using System;
 using UnityEngine;
 using System.Collections.Generic;
 using System.Linq;
-using RosSharp.RosBridgeClient;
-using RosSharp.RosBridgeClient.MessageTypes.Std;
-using RosUtils;
 using Unity.Robotics.UrdfImporter;
 using RosMessageTypes.Sensor;
 using RosMessageTypes.Std;
 using Unity.Robotics.ROSTCPConnector;
-using Joint = UnityEngine.Joint;
 
 public class Rover : MonoBehaviour
 {
@@ -17,87 +24,72 @@ public class Rover : MonoBehaviour
 
 	public float pivotForce = 10000f;
 	public float wheelForce = 10000f;
-	private ArticulationBody flp, blp, frp, brp;
-    private ArticulationBody blw, flw, brw, frw;
+	
+    string[] wheelNames = { "flw", "blw", "frw", "brw" };
+    string[] pivotNames = { "flp", "blp", "frp", "brp" };
 
-	private JointStateMsg lastCommandMessage;
+    private JointStateMsg lastCommandMessage;
+    private Dictionary<string, ArticulationBody> articulationBodies;
+	
+	private Dictionary<string, ArticulationBody> GetArticulationBodies(string[] jointNames)
+	{
+		var result = new Dictionary<string, ArticulationBody>();
+		
+		UrdfJoint[] urdfJoints = GetComponentsInChildren<UrdfJoint>();
+		foreach (UrdfJoint urdfJoint in urdfJoints)
+		{
+			if (jointNames.Contains(urdfJoint.jointName))
+			{
+				result.Add(urdfJoint.jointName, urdfJoint.GetComponent<ArticulationBody>());
+			}
+		}
 
+		return result;
+	}
+	
     void Start()
     {
 	    ros = ROSConnection.GetOrCreateInstance();
-	    ros.RegisterPublisher<JointStateMsg>("/joint_states");
 	    ros.RegisterPublisher<JointStateMsg>("/topic_based_joint_states");
 	    ros.Subscribe<JointStateMsg>("/topic_based_joint_commands", JointCommandCallback);
 	    
-        blw = transform.Find("base_link/chassis/left_leg/bl_ankle/bl_wheel").GetComponent<ArticulationBody>();
-        flw = transform.Find("base_link/chassis/left_leg/fl_ankle/fl_wheel").GetComponent<ArticulationBody>();
-        brw = transform.Find("base_link/chassis/right_leg/br_ankle/br_wheel").GetComponent<ArticulationBody>();
-        frw = transform.Find("base_link/chassis/right_leg/fr_ankle/fr_wheel").GetComponent<ArticulationBody>();
-
-        blp = transform.Find("base_link/chassis/left_leg/bl_ankle").GetComponent<ArticulationBody>();
-        flp = transform.Find("base_link/chassis/left_leg/fl_ankle").GetComponent<ArticulationBody>();
-        brp = transform.Find("base_link/chassis/right_leg/br_ankle").GetComponent<ArticulationBody>();
-        frp = transform.Find("base_link/chassis/right_leg/fr_ankle").GetComponent<ArticulationBody>();
-        
-        //Set initial speed / position
-        ArticulationBody[] wheels = {flw, blw, frw, brw};
-        ArticulationBody[] pivots = {flp, blp, frp, brp};
- 
-        //Wheels
-        for (int i = 0; i < 4; i++)
-        {
-	        ArticulationDrive jointState = wheels[i].xDrive;
-	        jointState.forceLimit = wheelForce;
-	        jointState.targetVelocity = 0;
-	        jointState.driveType = ArticulationDriveType.Velocity;
-	        wheels[i].xDrive = jointState;
-        }
-
-        //Pivots
-        for (int i = 0; i < 4; i++)
-        {
-	        ArticulationDrive jointState = pivots[i].xDrive;
-	        jointState.forceLimit = pivotForce;
-	        jointState.target = 0;
-	        jointState.driveType = ArticulationDriveType.Target;
-	        pivots[i].xDrive = jointState;
-        }
+	    string[] jointNames = wheelNames.Concat(pivotNames).ToArray();
+	    articulationBodies = GetArticulationBodies(jointNames);
     }
 
     // Update is called once per frame
     void Update()
     {
-	    ArticulationBody[] wheels = {flw, blw, frw, brw};
-	    ArticulationBody[] pivots = {flp, blp, frp, brp};
-	    
-	    // ROS2 Control Command
+	    // Check ROS2 Control Command
 		if (lastCommandMessage is not null)
 		{
+			string[] jointNames = lastCommandMessage.name;
 			//Wheels
-			for (int i = 0; i < 4; i++)
+			foreach (string wheel in wheelNames)
 			{
-				ArticulationDrive jointState = wheels[i].xDrive;
+				int index = Array.IndexOf(jointNames, wheel);
+				ArticulationDrive jointState = articulationBodies[wheel].xDrive;
+				jointState.stiffness = 0.0f;
+				jointState.damping = 10000f;
             	jointState.forceLimit = wheelForce;
-            	jointState.targetVelocity = 180 / 3.14159f * (float)lastCommandMessage.velocity[i];
+            	jointState.targetVelocity = 180f / 3.14159f * (float)lastCommandMessage.velocity[index];
             	jointState.driveType = ArticulationDriveType.Velocity;
-            	wheels[i].xDrive = jointState;
+	            articulationBodies[wheel].xDrive = jointState;
     		}
 
 			//Pivots
-			for (int i = 0; i < 4; i++)
+			foreach (string pivot in pivotNames)
 			{
-				ArticulationDrive jointState = pivots[i].xDrive;
+				int index = Array.IndexOf(jointNames, pivot);
+				ArticulationDrive jointState = articulationBodies[pivot].xDrive;
             	jointState.forceLimit = pivotForce;
-            	jointState.target = 180f / 3.14159f * (float)lastCommandMessage.position[i];
+            	jointState.target = 180f / 3.14159f * (float)lastCommandMessage.position[index];
             	jointState.driveType = ArticulationDriveType.Target;
-            	pivots[i].xDrive = jointState;
+	            articulationBodies[pivot].xDrive = jointState;
     		}
 		}
-
-		string[] wheelNames = { "flw", "blw", "frw", "brw" };
-		string[] pivotNames = { "flp", "blp", "frp", "brp" };
 		
-		//ROS2 Control States
+		// Publish ROS2 Control States
 		var count = wheelNames.Length + pivotNames.Length;
 		JointStateMsg stateMessage = new();
 		stateMessage.header =  new HeaderMsg(TimePublisher.GetCurrentTime(), "");
@@ -109,17 +101,19 @@ public class Rover : MonoBehaviour
 		//Wheels
 		for (int i = 0; i < wheelNames.Length; i++)
 		{
-			stateMessage.name[i] = wheelNames[i];
-			stateMessage.position[i] = wheels[i].jointPosition[0];
-			stateMessage.velocity[i] = wheels[i].jointVelocity[0];
+			string wheel = wheelNames[i];
+			stateMessage.name[i] = wheel;
+			stateMessage.position[i] = articulationBodies[wheel].jointPosition[0];
+			stateMessage.velocity[i] = articulationBodies[wheel].jointVelocity[0];
 		}
 		
 		//Pivots
 		for (int i = 0; i < pivotNames.Length; i++)
 		{
-			stateMessage.name[wheelNames.Length + i] = pivotNames[i];
-			stateMessage.position[wheelNames.Length + i] = pivots[i].jointPosition[0];
-			stateMessage.velocity[wheelNames.Length + i] = pivots[i].jointVelocity[0];
+			string pivot = pivotNames[i];
+			stateMessage.name[wheelNames.Length + i] = pivot;
+			stateMessage.position[wheelNames.Length + i] = articulationBodies[pivot].jointPosition[0];
+			stateMessage.velocity[wheelNames.Length + i] = articulationBodies[pivot].jointVelocity[0];
 		}
 		
 		ros.Publish("/topic_based_joint_states", stateMessage);
