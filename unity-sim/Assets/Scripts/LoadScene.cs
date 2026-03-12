1af3082ddcccc2aba40e2fc356f191d511d36ec2
Monash Nova Rover <novaroverteam@monash.edu>
2026-03-12 18:29:12 +1100

    Added some things

---
+++ unity-sim/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[System.Serializable]
+public class NamedObject<T>
+{
+    public string name;
+    public T obj;
+}
+
+public class LoadScene : MonoBehaviour
+{
+    public List<NamedObject<string>> scenes;
+
+    public static string GetArg(string name, string defaultValue)
+    {
+        string argString = name + '=';
+        //string[] args = System.Environment.GetCommandLineArgs();
+        string[] args = { "robot=auto_mount", "scene=ARC2025" };
+
+        foreach (string arg in args)
+        {
+            if (arg.StartsWith(argString))
+            {
+                return arg.Substring(argString.Length);
+            }
+        }
+
+        return defaultValue;
+    }
+    void Start()
+    {
+        string sceneName = GetArg("scene", "ARC2025");
+        string scene = scenes.First(scene => scene.name == sceneName).obj;
+
+        SceneManager.LoadScene(scene);
+    }
+}
