36393004fe3e0551bee04f2a47217e3f8e8c27b1
Victor Bartlinski <vicbart08@gmail.com>
2026-07-13 20:21:24 +1000

    added default prefab option in editor

--- unity-sim/Assets/Scripts/Setup.cs
+++ unity-sim/Assets/Scripts/Setup.cs
@@ -6,10 +6,26 @@ using UnityEngine.SceneManagement;
 
 public class Setup : MonoBehaviour
 {
+    public string defaultPrefab;
     public List<NamedObject<GameObject>> robotPrefabs;
+
     void Start()
     {
-        string robotName = LoadScene.GetArg("robot", "default");
+        // Check if Default Prefab is valid
+        List<string> robotPrefabStrings = robotPrefabs.Select(prefab => prefab.name).ToList();
+        if (!robotPrefabStrings.Contains(defaultPrefab)) {
+            Debug.LogError("Default Prefab does not exist in Robot Prefabs");
+            return;
+        }
+        
+        // Check if Robot Prefab argument is valid
+        string robotName = LoadScene.GetArg("robot", defaultPrefab);
+        if (!robotPrefabStrings.Contains(robotName)) {
+            Debug.LogError("Robot argument does not exist in Robot Prefabs");
+            return;
+        }
+
+        // Load Robot Prefab
         GameObject prefab = robotPrefabs.First(prefab => prefab.name == robotName).obj;
         
         GameObject startPositionObject = GameObject.Find("robot_start_position");
